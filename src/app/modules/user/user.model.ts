d84418e7218499c59aa474215aeeb1371234dc98
MostakemHossain <mostakememon123@gmail.com>
2023-11-24 01:08:11 +0600

    user model added

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,28 +1,33 @@
-import { Model } from 'mongoose';
+import { model, Schema } from 'mongoose';
+import { TUser } from './user.interface';
 
-export type TUser = {
-  userId: string;
-  username: string;
-  password: string;
+const userSchema = new Schema<TUser>({
+  userId: { type: String, required: true, unique: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
   fullName: {
-    firstName: string;
-    lastName: string;
-  };
-  age: number;
-  email: string;
-  isActive: 'active' | 'inactive';
-  hobbies: string[];
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+  },
+  age: { type: Number, required: true },
+  email: { type: String, required: true },
+  isActive: { type: String, enum: ['active', 'inactive'], required: true },
+  hobbies: { type: [String], required: true },
   address: {
-    street: string;
-    city: string;
-    country: string;
-  };
-  orders: Array<{
-    productName: string;
-    price: number;
-    quantity: number;
-  }>;
+    street: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+  },
+  orders: {
+    type: [
+      {
+        productName: { type: String, required: true },
+        price: { type: Number, required: true },
+        quantity: { type: Number, required: true },
+      },
+    ],
+  },
  
-};
+});
 
-export type userModel = Model<TUser>;
+export const User = model<TUser>('User', userSchema);
